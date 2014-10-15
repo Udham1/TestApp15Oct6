6a94f1f4fddf749de2c2debd78edbb7ca29bcd39
unknown-user <unknown-user@10.71.34.1>
2014-10-15 17:26:31 +0530

    Initial check-in of module TestApp15Oct7

---
+++ TestApp15Oct7/EmptyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestApp15Oct7
+{
+	public class EmptyClass
+	{
+		public EmptyClass ()
+		{
+		}
+	}
+}
+
---
+++ TestApp15Oct7/EmptyClass1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestApp15Oct7
+{
+	public class EmptyClass1
+	{
+		public EmptyClass1 ()
+		{
+		}
+	}
+}
+
